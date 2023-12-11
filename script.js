f676feec84dce52b333ab99a5e89ea63b6d4a5cb
libian19 <libiansimpson19@gmail.com>
2023-12-10 21:37:41 -0400

    Inicio de construcción de la página de biografía personal

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const inactivo = document.querySelector('.inactivo')
 const inactive = document.querySelector('#inactive')
-inactivo.addEventListener('click', aviso)
+
+const elemParrafo = document.querySelector('#parrafo').value;
+
+
+/*
 inactive.addEventListener('click', aviso)
 
 function aviso(){
-    alert('Lo siento, página en construción :(')
+  alert('Lo siento, página en construción :(')
+}
+*/
+
+function parrafo(elemParrafo){
+let contar = 0;
+let newStr = "";
+  for (let i=0; i < elemParrafo.length; i++) {
+    contar++
+    if (contar === 499){
+      newStr = str.slice(0,499) + "..."
+    }
+  }
+  return newStr;
 }
